89282e263acccd75af8c05c51a310800f86b65f4
junglq <junglqq@gmail.com>
2020-09-03 21:11:53 +0300

    like

--- js/script.js
+++ js/script.js
@@ -109,4 +109,15 @@ $(document).ready(function () {
             animOnScroll();
         }, 300);
     }
+
+    function ibg(){
+
+        $.each($('.ibg'), function(index, val) {
+        if($(this).find('img').length>0){
+        $(this).css('background-image','url("'+$(this).find('img').attr('src')+'")');
+        }
+        });
+        }
+        
+        ibg();
 })
