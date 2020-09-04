81308d6566d2c65a603a7cbe5ec513bf7609ab93
junglq <junglqq@gmail.com>
2020-09-04 22:23:30 +0300

    js change

--- js/script.js
+++ js/script.js
@@ -112,11 +112,12 @@ $(document).ready(function () {
 
     function ibg() {
 
-        $.each($('.ibg'), function (index, val) {
-            if ($(this).find('img').length > 0) {
-                $(this).css('background-image', 'url("' + $(this).find('img').attr('src') + '")');
+        let ibg = document.querySelectorAll(".ibg");
+        for (var i = 0; i < ibg.length; i++) {
+            if (ibg[i].querySelector('img')) {
+                ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
             }
-        });
+        }
     }
 
     ibg();
